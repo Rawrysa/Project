3dbcf7ab24f4c52fdee203abea1b39f742cdb834
Galaletsang Modimola <578204@student.belgiumcampus.ac.za>
2022-07-21 21:50:29 +0200

    Put things in their respective folders

--- Program.cs
+++ Program.cs
@@ -16,8 +16,7 @@ namespace Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomePage());
-            //Application.Run(new SlashScreen());
+            Application.Run(new Login());
         }
     }
 }
