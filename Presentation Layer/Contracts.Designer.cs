6c5b74bed1baf4543452c7b74331a5e833990d1b
Karabo <karaboelijahkoka26@gmail.com>
2022-07-22 19:55:49 +0200

    Hopefully this works.

--- Presentation Layer/Contracts.Designer.cs
+++ Presentation Layer/Contracts.Designer.cs
@@ -34,12 +34,14 @@
             this.dgrContracts = new System.Windows.Forms.DataGridView();
             this.btnBack = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.lblContract = new System.Windows.Forms.Label();
+            this.TxtContracts = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgrContracts)).BeginInit();
             this.SuspendLayout();
             // 
             // btnViewContracts
             // 
-            this.btnViewContracts.Location = new System.Drawing.Point(55, 339);
+            this.btnViewContracts.Location = new System.Drawing.Point(56, 382);
             this.btnViewContracts.Name = "btnViewContracts";
             this.btnViewContracts.Size = new System.Drawing.Size(136, 41);
             this.btnViewContracts.TabIndex = 0;
@@ -49,9 +51,9 @@
             // 
             // btnContractAvailability
             // 
-            this.btnContractAvailability.Location = new System.Drawing.Point(307, 339);
+            this.btnContractAvailability.Location = new System.Drawing.Point(274, 382);
             this.btnContractAvailability.Name = "btnContractAvailability";
-            this.btnContractAvailability.Size = new System.Drawing.Size(148, 41);
+            this.btnContractAvailability.Size = new System.Drawing.Size(153, 41);
             this.btnContractAvailability.TabIndex = 1;
             this.btnContractAvailability.Text = "Contract Availability";
             this.btnContractAvailability.UseVisualStyleBackColor = true;
@@ -59,7 +61,7 @@
             // 
             // btnContractPerformance
             // 
-            this.btnContractPerformance.Location = new System.Drawing.Point(556, 339);
+            this.btnContractPerformance.Location = new System.Drawing.Point(495, 382);
             this.btnContractPerformance.Name = "btnContractPerformance";
             this.btnContractPerformance.Size = new System.Drawing.Size(168, 41);
             this.btnContractPerformance.TabIndex = 2;
@@ -70,7 +72,7 @@
             // dgrContracts
             // 
             this.dgrContracts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgrContracts.Location = new System.Drawing.Point(41, 13);
+            this.dgrContracts.Location = new System.Drawing.Point(56, 65);
             this.dgrContracts.Name = "dgrContracts";
             this.dgrContracts.RowHeadersWidth = 51;
             this.dgrContracts.RowTemplate.Height = 24;
@@ -79,7 +81,7 @@
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(673, 45);
+            this.btnBack.Location = new System.Drawing.Point(689, 90);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(98, 41);
             this.btnBack.TabIndex = 4;
@@ -89,7 +91,7 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(673, 220);
+            this.btnExit.Location = new System.Drawing.Point(689, 296);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(98, 41);
             this.btnExit.TabIndex = 5;
@@ -97,11 +99,31 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // lblContract
+            // 
+            this.lblContract.AutoSize = true;
+            this.lblContract.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContract.Location = new System.Drawing.Point(147, 27);
+            this.lblContract.Name = "lblContract";
+            this.lblContract.Size = new System.Drawing.Size(163, 22);
+            this.lblContract.TabIndex = 6;
+            this.lblContract.Text = "Enter Contract ID : ";
+            this.lblContract.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TxtContracts
+            // 
+            this.TxtContracts.Location = new System.Drawing.Point(334, 27);
+            this.TxtContracts.Name = "TxtContracts";
+            this.TxtContracts.Size = new System.Drawing.Size(275, 22);
+            this.TxtContracts.TabIndex = 7;
+            // 
             // FrmContracts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 421);
+            this.ClientSize = new System.Drawing.Size(818, 450);
+            this.Controls.Add(this.TxtContracts);
+            this.Controls.Add(this.lblContract);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.dgrContracts);
@@ -112,6 +134,7 @@
             this.Text = "Contracts";
             ((System.ComponentModel.ISupportInitialize)(this.dgrContracts)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -123,5 +146,7 @@
         private System.Windows.Forms.DataGridView dgrContracts;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblContract;
+        private System.Windows.Forms.TextBox TxtContracts;
     }
 }
